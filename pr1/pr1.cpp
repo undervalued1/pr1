ce3bb186dec79107d4a10bc6ce3dc308e95363c5
undervalued1 <157449229+undervalued1@users.noreply.github.com>
2024-09-21 11:23:50 +0500

    Add files via upload

---
+++ pr1/pr1.cpp
@@ -0,0 +1,33 @@
+﻿// pr1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main() 
+{//zad1
+    int n, max;
+    std::cout << "n = ";
+    std::cin >> n;
+    max = 0;
+    while (n) 
+    {
+        if (n % 10 > max)
+        {
+            max = n % 10;
+        }
+        n /= 10;
+    }
+    std::cout << "Result: " << max << "\n";
+ //zad2
+    int a, b;
+    std::cout << "a=";
+    std::cin >> a;
+    std::cout << "b=";
+    std::cin >> b;
+    a = a +b;
+    b = a-b;
+    a = a-b;
+    std::cout << a; "\n";
+    std::cout << b;
+}
+
---
+++ pr1/push.cmd
@@ -0,0 +1,7 @@
+echo "# pr1" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/undervalued1/pr1.git
+git push -u origin main
